e26863afe8eb81f84cb65a20ccdc4e80cb991315
Baka <neoplanspaceliner9@gmail.com>
2016-08-28 04:32:00 +0300

    Update SQLiteStorage methods: add error logging

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var path = require('path');
 var mongoose = require('mongoose');
+var sqlite3 = require('sqlite3');
 
 var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -14,24 +15,77 @@ var upload = multer(); // for parsing multipart/form-data
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+pp.use(express.static(__dirname + '/')); 
+
+
+
+//===========================SQLITE============================
+
+function SQLiteStorage(pathToDB) {
+	this.db = new sqlite3.Database(pathToDB);
+	return this;
+};
+
+SQLiteStorage.prototype.getPosts = function(from, to, callback) {
+	var db = this.db;
+	db.serialize(function() {
+		db.all('SELECT * FROM Posts LIMIT ' + to + ' OFFSET ' + from, function(err, rows) {
+			if (err) console.log('SQL Error:', err);
+			if (typeof callback == "function") {
+				callback(rows);
+			}
+		});
+	});
+};
+
+SQLiteStorage.prototype.addPost = function(content, callback) {
+	var db = this.db;
+	var date = getDateString();
+	var time = getTimeString();
+	db.serialize(function() {
+	    var query = "INSERT INTO Posts (date, time, content) VALUES (?, ?, ?)";
+		db.run(query, date, time, content, function (err) {
+			if (err) console.log('SQL Error:', err);
+			if (typeof callback == "function") {
+				callback();
+			}
+		});
+	});
+};
+
+SQLiteStorage.prototype.countPosts = function(callback) {
+	var query = "SELECT COUNT(*) FROM Posts;";
+	db.run(query, function (err, rows) {
+		callback && callback (rows['COUNT ']); 
+		if (err) console.log('SQL Error:', err);
+	});
+};
+
 
 
 //===========================MONGODB============================
 
-// Connect to MongoDB and create/use database called bChat
-mongoose.connect('mongodb://localhost/bChat');
-
 // Create a schema
 var bChatSchema = new mongoose.Schema({
-  message: String,
-  date: String,
-  time: String,
+	message: String,
+	date: String,
+	time: String,
 });
 
-// Create a model based on the schema
-var bChatModel = mongoose.model('bChat', bChatSchema);
+function Mongo(dbName, schema) {
+	// Connect to MongoDB and create/use database called <dbName>
+	mongoose.connect('mongodb://localhost/' + dbName);
+	// Create a model based on the schema
+	this.model = mongoose.model(dbName, schema);
+	return this;
+};
 
-
+Mongo.prototype.addPost = function(content, callback) {
+	var model = this.model;
+	model.create({message: content, date: date, time: time}, function(err, todo){
+  if(err) console.log(err);
+  else console.log(todo);
+});
 
 //====================================================================
 
